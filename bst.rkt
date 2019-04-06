fa8dc838a733246d6cc1dc975d5061185fbe1fce
Mahdi H Mohamed <mr.mahdimohamediii@gmail.com>
2019-04-06 08:52:28 -0500

    Binary search tree

---
+++ bst.rkt
@@ -0,0 +1,70 @@
+
+
+( define entry
+  (lambda (currentList  )
+   (car currentList )   
+  )
+)
+
+( define left 
+  (lambda (currentList  )
+   
+   (car (cdr  currentList ) )
+  )
+)
+
+( define right 
+  (lambda (currentList  )
+  
+   (car(cdr (cdr currentList ) ) )
+  )
+)
+
+
+ 
+( define preorder
+  (lambda (currentList  )
+    (cond
+      ((null? currentList) '() )
+      (else  (append (list (entry currentList)) ( preorder (left currentList )) ( preorder (right currentList )) ))
+     )  
+  )
+)
+
+
+( define inorder
+  (lambda (currentList  )
+    (cond
+      ((null? currentList) '() )
+      (else  (append  ( inorder (left currentList )) (list (entry currentList)) ( inorder (right currentList )) ))
+     )  
+  )
+)
+
+
+( define postorder
+  (lambda (currentList  )
+    (cond
+      ((null? currentList) '() )
+      (else  (append  ( postorder (left currentList )) ( postorder (right currentList ))  (list (entry currentList)) ))
+     )  
+  )
+)
+
+
+
+
+( define x
+   '(
+  5
+  (3 (12 () ()) (4 () (22 () () )) )
+  (43 () () )
+  
+ ))
+
+(entry x)
+(left x)
+(right x)
+(preorder x) 
+(inorder x)
+(postorder x)
