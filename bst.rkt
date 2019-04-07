9a0d5cac4cd8770550051666e0a87dbd26d8d793
Mahdi H Mohamed <mr.mahdimohamediii@gmail.com>
2019-04-07 13:54:06 -0500

    added comments

--- bst.rkt
+++ bst.rkt
@@ -26,7 +26,8 @@
   (lambda (currentList  )
     (cond
       ((null? currentList) '() )
-      (else  (append (list (entry currentList)) ( preorder (left currentList )) ( preorder (right currentList )) ))
+      ;; for each node: it adds the node, the values to the left, and the values to the right
+      (else  (append (list (entry currentList))   ( preorder (left currentList ))    ( preorder (right currentList )) ))
      )  
   )
 )
@@ -36,7 +37,8 @@
   (lambda (currentList  )
     (cond
       ((null? currentList) '() )
-      (else  (append  ( inorder (left currentList )) (list (entry currentList)) ( inorder (right currentList )) ))
+      ;; for each node: it adds the values to the left, the node, and the values to the right
+      (else  (append  ( inorder (left currentList ))    (list (entry currentList))    ( inorder (right currentList )) ))
      )  
   )
 )
@@ -46,7 +48,8 @@
   (lambda (currentList  )
     (cond
       ((null? currentList) '() )
-      (else  (append  ( postorder (left currentList )) ( postorder (right currentList ))  (list (entry currentList)) ))
+      ;; for each node: it adds the values to the left, the values to the right, and the node
+      (else  (append  ( postorder (left currentList ))   ( postorder (right currentList ))    (list (entry currentList)) ))
      )  
   )
 )
@@ -55,16 +58,16 @@
 (define (insert v currentList)
   (cond
     
-    
-    ((equal? currentList '()) (list v '() '()) )
-    
-    ((> v (entry currentList)) (list (car currentList) (car (cdr currentList)) (insert v (car (cdr (cdr currentList))))))
-    
-    (else (list (car currentList) (insert v (car (cdr currentList))) (car (cdr (cdr currentList)))))
+    ;; if it hits a leaf, insert bst with the value as its only node
+    ((equal? currentList '())   (list v '() '()) )
+
+    ;;if bigger return a  list of the node, left side of the tree, and call insert on right side
+    ((> v (entry currentList))   (list (car currentList)   (car (cdr currentList))   (insert v (car (cdr (cdr currentList))))))
+
+    ;; if smaller return a list of the node, call insert of left side, and the right side of the tree,
+    (else (list (car currentList)    (insert v (car (cdr currentList)))   (car (cdr (cdr currentList)))))
   )  
 )
-
-
 
 
 
@@ -87,6 +90,7 @@
 (preorder x) 
 (inorder x)
 (postorder x)
+x
 (insert 6 x)
 
 ;; 5
@@ -95,4 +99,5 @@
 ;; (5 3 12 4 22 43)
 ;; (12 3 4 22 5 43)
 ;; (12 22 4 3 43 5)
+;; (5 (3 (12 () ()) (4 () (22 () ()))) (43 () ()))
 ;; (5 (3 (12 () ()) (4 () (22 () ()))) (43 (6 () ()) ()))
