e6459da8c5d211ac6022a2865bf425c570fd0dfb
Mahdi H Mohamed <mr.mahdimohamediii@gmail.com>
2019-04-11 14:31:11 -0500

    implemented my-filter function

--- higherorder.rkt
+++ higherorder.rkt
@@ -1,14 +1,29 @@
-(define mult
-  (lambda (a)
-    (lambda (b)
-      (* a b))))
+;; 4/11/19
+;; CS 251
+;; Currying and higher order functions
+
+
+;; Currying
 
 (define curry3
   (lambda (a)
     (lambda (b)
       (lambda (c)
-        (list a b c)
-       )
-      )
-   )
-)
+        (lambda (d)
+          (list a b c d ))))))
+
+(define uncurry3
+  (lambda (f)
+    (define a (car (((f null) null) null)))
+    (lambda ( x y z)
+      (list a x y z))))
+
+;; Higher order functions
+
+(define my-filter
+  (lambda (f lst)
+    (cond
+      ((null? lst) '())
+      ((equal? (f (car lst)) #f) (my-filter f (cdr lst)) )
+      (else (append (list (car lst)) (my-filter f (cdr lst)) )))))
+
