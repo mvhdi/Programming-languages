4e7035daa2a2c36d2f0dfde14ee600997e2b72fe
Mahdi H Mohamed <mr.mahdimohamediii@gmail.com>
2019-04-15 13:38:26 -0500

    implemented prime function

--- sieve.rkt
+++ sieve.rkt
@@ -33,47 +33,41 @@
     (else (car (reverse (first-n lazy-list n))) ))))
 
 
-(define return-all
-  (lambda (lazy-list)
-    (cond
-      ((equal? lazy-list #f) '())
-      (else (append (list (car lazy-list)) (return-all (call-lazy-list lazy-list))) )
-      
-    )))
+
 
 (define is-multiple
   (lambda (val1)
     (lambda (val2)
     (cond
-      ((integer? (/ val2 val1)) #t)
-      (else #f)
-     ))))
-
-
-(define filter-multiples
-  (lambda (lazy-list n)
-    (filter  (is-multiple n ) (return-all lazy-list) )))
+      ((integer? (/ val1 val2)) #t)
+      (else #f)))))
 
 
 
-(define sieve
-  (lambda (lazy-list)
-   ( lazy-list-from-lst (filter-multiples  (call-lazy-list lazy-list) (car lazy-list)  ) )
-    ))
-
-(define lazy-list-from-lst
-  (lambda (lst)
-    (if (null? lst)
-        #f
-        (cons (car lst)
-            (lambda ()
-              (lazy-list-from-lst (cdr lst)))))))
 
 (define primes
   (lambda ()
-       (sieve (inf-seq 2))
-    ))
+    (primes-helper 2 '() )))
 
+
+(define primes-helper
+  (lambda (start lst)
+        (cons (car (prime-finder start lst))
+            (lambda ()
+              (primes-helper (+ (car (prime-finder start lst)) 1) (prime-finder start lst))))))
+
+(define prime-finder
+  (lambda (start lst)
+    (cond
+      ((check-prime start lst) (append (list start) lst))
+      (else (prime-finder (+ start 1) lst) ))))
+
+
+(define check-prime
+  (lambda (val lst)
+    (cond
+      ( (null? (filter (is-multiple val ) lst ) )    #t)
+      (else #f))))
 
 
 ;; tests 
@@ -87,10 +81,9 @@
 (nth x 10)
 (nth x 50)
 (nth x 0)
-(return-all x)
 
+(prime-finder 4 '(3 2))
+(check-prime 4 '(3 2))
 
-
-(filter-multiples x 2)
-(filter-multiples x 3)
-
+(first-n (primes) 10)
+(nth (primes) 20)
